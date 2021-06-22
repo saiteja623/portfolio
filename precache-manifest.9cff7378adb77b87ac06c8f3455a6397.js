94e01a0295fa646c5bd45ad5851971063469b3d6
saiteja623 <saiteja.balla13@gmail.com>
2021-06-22 22:11:19 +0530

    first-commit

---
+++ precache-manifest.9cff7378adb77b87ac06c8f3455a6397.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d501f3ca7185ca5ad5dc3069d442be",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4e6a14a6812bfa8dea97",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "27cd631bc18f59595b9c",
+    "url": "/portfolio/static/css/main.32cb4d01.chunk.css"
+  },
+  {
+    "revision": "4e6a14a6812bfa8dea97",
+    "url": "/portfolio/static/js/2.6dbdcc9e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.6dbdcc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27cd631bc18f59595b9c",
+    "url": "/portfolio/static/js/main.bef55550.chunk.js"
+  },
+  {
+    "revision": "111da7d6b1328ab36ba7",
+    "url": "/portfolio/static/js/runtime-main.dac44458.js"
+  },
+  {
+    "revision": "47e22166ad418fb55f3465102281e6d7",
+    "url": "/portfolio/static/media/chatapp.47e22166.jpeg"
+  },
+  {
+    "revision": "4a49ab41e8be0645f84815484fa46f3c",
+    "url": "/portfolio/static/media/ecom.4a49ab41.jpeg"
+  },
+  {
+    "revision": "a9ccfee1b4fc3e22b3f9e06fcb59ca7b",
+    "url": "/portfolio/static/media/games2.a9ccfee1.jpeg"
+  },
+  {
+    "revision": "a78dc4a6dc34a338bcd9b0d423287f04",
+    "url": "/portfolio/static/media/instagram.a78dc4a6.jpeg"
+  },
+  {
+    "revision": "24dbd379d79187a9b5c4b8f45c90c542",
+    "url": "/portfolio/static/media/music.24dbd379.png"
+  },
+  {
+    "revision": "f1f268d2df8c8ff27c8ef5aea773de91",
+    "url": "/portfolio/static/media/news.f1f268d2.png"
+  },
+  {
+    "revision": "a93b480d653abb61f6b8319c2664b1d8",
+    "url": "/portfolio/static/media/profile.a93b480d.png"
+  }
+]);
